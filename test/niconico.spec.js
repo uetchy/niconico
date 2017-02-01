09ec70fdb68d2838736047577425b38911736ed0
Yasuaki Uechi <uetchy@randompaper.co>
2017-02-01 14:16:07 +0900

    style: add flow

--- test/niconico.spec.js
+++ test/niconico.spec.js
@@ -1,42 +1,42 @@
-import test from 'ava';
+import test from 'ava'
 
-import {niconico} from '..';
+import {niconico} from '..'
 
 test.beforeEach(t => {
-	t.context.videoID = process.env.VIDEO_ID || 'sm28222588';
-});
+  t.context.videoID = process.env.VIDEO_ID || 'sm28222588'
+})
 
 test('success to sign in', t => {
-	return niconico
-		.login(process.env.EMAIL, process.env.PASSWORD)
-		.then(session => {
-			// console.log('session:', session);
-			const json = session
-				.getCookies('http://nicovideo.jp')
-				.map(l => l.toJSON())
-				.map(c => c.key);
+  return niconico
+    .login(process.env.EMAIL, process.env.PASSWORD)
+    .then(session => {
+      // console.log('session:', session)
+      const json = session
+        .getCookies('http://nicovideo.jp')
+        .map(l => l.toJSON())
+        .map(c => c.key)
 
-			t.true(json.includes('nicosid'));
-			t.true(json.includes('user_session'));
-		})
-		.catch(err => {
-			console.log(err);
-		});
-});
+      t.true(json.includes('nicosid'))
+      t.true(json.includes('user_session'))
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 test('fail to sign in', t => {
-	return niconico
-		.login(process.env.EMAIL, 'invalidpassword')
-		.then(session => {
-			const json = session
-				.getCookies('http://nicovideo.jp')
-				.map(l => l.toJSON())
-				.map(c => c.key);
+  return niconico
+    .login(process.env.EMAIL, 'invalidpassword')
+    .then(session => {
+      const json = session
+        .getCookies('http://nicovideo.jp')
+        .map(l => l.toJSON())
+        .map(c => c.key)
 
-			t.true(json.includes('nicosid'));
-			t.false(json.includes('user_session'));
-		})
-		.catch(err => {
-			console.log(err);
-		});
-});
+      t.true(json.includes('nicosid'))
+      t.false(json.includes('user_session'))
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
--- test/nicovideo.spec.js
+++ test/nicovideo.spec.js
@@ -1,57 +1,57 @@
-import test from 'ava';
+import test from 'ava'
 
-import {niconico, Nicovideo} from '..';
+import {niconico, Nicovideo} from '..'
 
-const EMAIL = process.env.EMAIL;
-const PASSWORD = process.env.PASSWORD;
-const VIDEO_ID = 'sm28222588';
+const EMAIL = process.env.EMAIL
+const PASSWORD = process.env.PASSWORD
+const VIDEO_ID = 'sm28222588'
 
 test('get watch data containers', t => {
-	return niconico
-		.login(EMAIL, PASSWORD)
-		.then(session => {
-			return new Nicovideo(session).watch(VIDEO_ID);
-		})
-		.then(result => {
-			// console.log('watchAPI:', result);
-			t.is(result.watchAPI.videoDetail.title, '【ゆめにっき】クリプト・オブ･ザ・モノクロダンサー');
-		})
-		.catch(err => {
-			console.log(err);
-		});
-});
+  return niconico
+    .login(EMAIL, PASSWORD)
+    .then(session => {
+      return new Nicovideo(session).watch(VIDEO_ID)
+    })
+    .then(result => {
+      // console.log('watchAPI:', result)
+      t.is(result.watchAPI.videoDetail.title, '【ゆめにっき】クリプト・オブ･ザ・モノクロダンサー')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 test('getthumbinfo', t => {
-	return new Nicovideo()
-		.thumbinfo(VIDEO_ID)
-		.then(thumbinfo => {
-			// console.log('thumbinfo:', thumbinfo);
-			t.is(thumbinfo.watchURL, `http://www.nicovideo.jp/watch/${VIDEO_ID}`);
-		})
-		.catch(err => {
-			console.log(err);
-		});
-});
+  return new Nicovideo()
+    .thumbinfo(VIDEO_ID)
+    .then(thumbinfo => {
+      // console.log('thumbinfo:', thumbinfo)
+      t.is(thumbinfo.watchURL, `http://www.nicovideo.jp/watch/${VIDEO_ID}`)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 test('download videos', () => {
-	return niconico
-		.login(EMAIL, PASSWORD)
-		.then(session => {
-			return new Nicovideo(session).download(VIDEO_ID, '.');
-		})
-		.then(filePath => {
-			console.log(filePath);
-		})
-		.catch(err => {
-			console.log(err);
-		});
-});
+  return niconico
+    .login(EMAIL, PASSWORD)
+    .then(session => {
+      return new Nicovideo(session).download(VIDEO_ID, '.')
+    })
+    .then(filePath => {
+      console.log(filePath)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 test('should fail when can not logIn', t => {
-	return niconico
-		.login('', '')
-		.then(() => new Nicovideo().watch(VIDEO_ID))
-		.then(() => t.fail())
-		.catch(() => t.pass());
-});
+  return niconico
+    .login('', '')
+    .then(() => new Nicovideo().watch(VIDEO_ID))
+    .then(() => t.fail())
+    .catch(() => t.pass())
+})
 
