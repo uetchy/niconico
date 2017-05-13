dc8b2f7fe50fa167dd69e5eb9f80d55f49eebc15
Yasuaki Uechi <uetchy@randompaper.co>
2017-05-13 11:44:24 +0900

    chore: 100% coverage

--- test/niconico.spec.js
+++ test/niconico.spec.js
@@ -6,37 +6,22 @@ test.beforeEach(t => {
   t.context.videoID = process.env.VIDEO_ID || 'sm28222588'
 })
 
-test('success to sign in', t => {
-  return niconico
-    .login(process.env.EMAIL, process.env.PASSWORD)
-    .then(session => {
-      // console.log('session:', session)
-      const json = session
-        .getCookies('http://nicovideo.jp')
-        .map(l => l.toJSON())
-        .map(c => c.key)
+test('success to sign in', async t => {
+  const session = await niconico.login(process.env.EMAIL, process.env.PASSWORD)
+  const json = session
+    .getCookies('http://nicovideo.jp')
+    .map(l => l.toJSON())
+    .map(c => c.key)
 
-      t.true(json.includes('nicosid'))
-      t.true(json.includes('user_session'))
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  t.true(json.includes('nicosid'))
+  t.true(json.includes('user_session'))
 })
 
-test('fail to sign in', t => {
-  return niconico
-    .login(process.env.EMAIL, 'invalidpassword')
-    .then(session => {
-      const json = session
-        .getCookies('http://nicovideo.jp')
-        .map(l => l.toJSON())
-        .map(c => c.key)
-
-      t.true(json.includes('nicosid'))
-      t.false(json.includes('user_session'))
-    })
-    .catch(err => {
-      console.log(err)
-    })
+test('fail to sign in', async t => {
+  try {
+    const session = await niconico.login(process.env.EMAIL, 'invalidpassword')
+    t.fail(session)
+  } catch (err) {
+    t.pass()
+  }
 })
--- test/nicovideo.spec.js
+++ test/nicovideo.spec.js
@@ -6,51 +6,65 @@ const EMAIL = process.env.EMAIL
 const PASSWORD = process.env.PASSWORD
 const VIDEO_ID = 'sm28222588'
 
-test('get watch data containers', t => {
-  return niconico
-    .login(EMAIL, PASSWORD)
-    .then(session => {
-      return new Nicovideo(session).watch(VIDEO_ID)
-    })
-    .then(result => {
-      // console.log('watchAPI:', result)
-      t.is(result.watchAPI.videoDetail.title, '【ゆめにっき】クリプト・オブ･ザ・モノクロダンサー')
-    })
+let client
+
+test.before(async t => {
+  const session = await niconico.login(EMAIL, PASSWORD)
+  client = new Nicovideo(session)
+})
+
+test('get watch data containers', async t => {
+  const result = await client.watch(VIDEO_ID)
+  t.is(result.watchAPI.videoDetail.title, '【ゆめにっき】クリプト・オブ･ザ・モノクロダンサー')
+})
+
+test('fail when invalid videoID given', t => {
+  return client
+    .watch('sm99999999999')
+    .then(result => t.fail(result))
     .catch(err => {
-      console.log(err)
+      t.pass(err)
     })
 })
 
-test('getthumbinfo', t => {
-  return new Nicovideo()
-    .thumbinfo(VIDEO_ID)
-    .then(thumbinfo => {
-      // console.log('thumbinfo:', thumbinfo)
-      t.is(thumbinfo.watchURL, `http://www.nicovideo.jp/watch/${VIDEO_ID}`)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+test('fail when credentials missing', async t => {
+  try {
+    const session = await niconico.login('', '')
+    const result = await new Nicovideo(session).watch(VIDEO_ID)
+    t.fail(result)
+  } catch (err) {
+    t.pass(err)
+  }
 })
 
-test('download videos', () => {
-  return niconico
-    .login(EMAIL, PASSWORD)
-    .then(session => {
-      return new Nicovideo(session).download(VIDEO_ID, '.')
-    })
-    .then(filePath => {
-      console.log(filePath)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+test('getthumbinfo', async t => {
+  try {
+    const thumbinfo = await new Nicovideo().thumbinfo(VIDEO_ID)
+    t.is(thumbinfo.watchURL, `http://www.nicovideo.jp/watch/${VIDEO_ID}`)
+  } catch (err) {
+    t.fail(err)
+  }
 })
 
-test('should fail when can not logIn', t => {
-  return niconico
-    .login('', '')
-    .then(() => new Nicovideo().watch(VIDEO_ID))
-    .then(() => t.fail())
-    .catch(() => t.pass())
+test('invalid getthumbinfo', async t => {
+  try {
+    const thumbinfo = await new Nicovideo().thumbinfo('sm99999999999')
+    t.fail(thumbinfo)
+  } catch (err) {
+    t.pass()
+  }
+})
+
+test('fail to download video', async t => {
+  try {
+    const filePath = await client.download('sm99999999999', '.')
+    t.fail(filepath)
+  } catch (err) {
+    t.pass()
+  }
+})
+
+test('download video', async t => {
+  const filePath = await client.download(VIDEO_ID, '.')
+  t.pass()
 })
