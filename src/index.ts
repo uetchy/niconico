8fcbfe19f04353c9b52e56eb8e165a745ee4c0c1
Yasuaki Uechi <y@uechi.io>
2019-01-09 21:50:35 +0900

    cosmetic changes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import * as niconico from './niconico'
 import Nicovideo from './nicovideo'
+
 export { niconico, Nicovideo }
--- src/niconico.ts
+++ src/niconico.ts
@@ -1,31 +1,30 @@
 import request from 'request'
 import { post } from 'request-promise'
 
-export function login(email: string, password: string) {
-  return new Promise<request.CookieJar>(async (resolve, reject) => {
-    const requestURL =
-      'https://account.nicovideo.jp/api/v1/login?site=niconico&next_url='
-    const jar = request.jar()
-    try {
-      const res = await post(requestURL, {
-        jar: jar,
-        form: {
-          mail_tel: email,
-          password: password,
-        },
-        simple: false,
-        resolveWithFullResponse: true,
-      })
+export async function login(
+  email: string,
+  password: string
+): Promise<request.CookieJar> {
+  const requestURL =
+    'https://account.nicovideo.jp/api/v1/login?site=niconico&next_url='
+  const jar = request.jar()
+  try {
+    const res = await post(requestURL, {
+      jar: jar,
+      form: {
+        mail_tel: email,
+        password: password,
+      },
+      simple: false,
+      resolveWithFullResponse: true,
+    })
 
-      if (
-        !jar.getCookieString('https://nicovideo.jp').includes('user_session')
-      ) {
-        return reject('invalid credentials')
-      }
-
-      resolve(jar)
-    } catch (err) {
-      reject(err)
+    if (!jar.getCookieString('https://nicovideo.jp').includes('user_session')) {
+      throw new Error('invalid credentials')
     }
-  })
+
+    return jar
+  } catch (err) {
+    throw new Error(err)
+  }
 }
